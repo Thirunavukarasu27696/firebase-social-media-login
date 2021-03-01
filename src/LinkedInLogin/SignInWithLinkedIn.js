feece90f9d7ab61dcb2c7e8ffd1c221e57138b30
Thirunavukarasu s <thirunavukarasu27696@gmail.com>
2021-03-01 18:03:45 +0530

    linked redirect url done

--- src/LinkedInLogin/SignInWithLinkedIn.js
+++ src/LinkedInLogin/SignInWithLinkedIn.js
@@ -11,6 +11,7 @@ class SignInWithLinkedIn extends Component {
         this.state = {
             code: '',
             errorMessage: '',
+            redirectURL: `https://firebase-social-media-login.herokuapp.com/linkedin`
         }
         // this.handleSuccess.bind(this);
         // this.handleFailure.bind(this);
@@ -45,7 +46,7 @@ class SignInWithLinkedIn extends Component {
 
     getAccessToken(code) {
         // const { code } = this.state;
-        const url = `https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&redirect_uri=http://localhost:3000/linkedin&client_id=86fl22mto3b92u&client_secret=FHeTaubxVPzl7WJN&code=${code}`;
+        const url = `https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&redirect_uri=${this.state.redirectURL}/linkedin&client_id=86fl22mto3b92u&client_secret=FHeTaubxVPzl7WJN&code=${code}`;
 
         APIService.get(url, null).then(response => {
             console.log(response);
@@ -55,18 +56,19 @@ class SignInWithLinkedIn extends Component {
     }
 
     render() {
-        const { code, errorMessage } = this.state;
+        const { code, errorMessage, redirectURL } = this.state;
+
         return (
             <div>
                 <LinkedIn
                     clientId="86fl22mto3b92u"
                     onFailure={e => this.handleFailure(e)}
                     onSuccess={e => this.handleSuccess(e)}
-                    redirectUri="http://localhost:3000/linkedin"
+                    redirectUri={redirectURL}
                 >
                     <img src={linkedin} alt="Log in with Linked In" style={{ maxWidth: '180px' }} />
                 </LinkedIn>
-                
+
                 {!code && <div>No code</div>}
                 {code && <div>Code: {code}</div>}
                 {errorMessage && <div>{errorMessage}</div>}
